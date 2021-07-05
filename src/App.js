6aac313134b42b96e529af792a60811fb2d88540
Dushime Emani Elvis <51426@etu.he2b.be>
2021-07-05 12:27:30 +0200

    Todolist_Elvis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import  'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className="App">
       <nav>
-
-
-
         <div>
           <a href=""></a>
           <a href=""></a>
@@ -19,19 +16,14 @@ function App() {
           <a href=""></a>
         </div>
 
-
-
         <div>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
         </div>
-
-
-
-
-
       </nav>
+
+      <TodoList />
     </div>
   );
 }
